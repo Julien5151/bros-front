247b90c5b15bd31ba983b43b2fc5d16490466489
Julien CLAUDEL <jclaudel@enovacom.fr>
2021-06-30 10:17:09 +0200

    Added react material and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.scss";
+import { Button } from "@material-ui/core";
 
 function App() {
-    return <h1>Bros front !</h1>;
+    return (
+        <div className="bros-application">
+            <h1>Bros front !</h1>
+            <Button variant="contained" color="primary">
+                Primary
+            </Button>
+        </div>
+    );
 }
 
 export default App;
